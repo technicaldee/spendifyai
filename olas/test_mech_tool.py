9df5efac9f88bb32a216f91d58e1b76deecbb8a6
Edidiong Udoh <gisthive@gmail.com>
2024-04-23 05:00:15 +0100

    fixes

--- olas/test_mech_tool.py
+++ olas/test_mech_tool.py
@@ -1,31 +1,23 @@
-from flask import Flask, jsonify, request
 from mech_client.interact import interact, ConfirmationType
 import json
 
-app = Flask(__name__)
+prompt_text = "Will the Federal Reserve impose interest rate cuts by 12 April 2024?"
+tool_name = "prediction-online"
 
-@app.route('/', methods=['POST'])
-def get_prediction():
-    try:
-        data = request.get_json()
-        prompt_text = data.get('prompt_text')
-        agent_id = data.get('agent_id', 2)  
-        tool_name = data.get('tool_name', 'openai_request') 
-        chain_config = data.get('chain_config', 'celo') 
-        private_key_path = data.get('private_key_path', 'ethereum_private_key.txt') 
-        response = interact(
-            prompt=prompt_text,
-            agent_id=agent_id,
-            tool=tool_name,
-            chain_config=chain_config,
-            confirmation_type=ConfirmationType.ON_CHAIN,
-            private_key_path=private_key_path
-        )
 
-        result = json.loads(response["result"])
-        return jsonify({'result': result}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+chain_config = "celo"
+agent_id = 2
 
-if __name__ == '__main__':
-    app.run(debug=True)
+private_key_path = "ethereum_private_key.txt"
+
+response = interact(
+    prompt=prompt_text,
+    agent_id=agent_id,
+    tool=tool_name,
+    chain_config=chain_config,
+    confirmation_type=ConfirmationType.ON_CHAIN,
+    private_key_path=private_key_path
+)
+
+print(json.dumps(response, indent=4))
+print(json.dumps(json.loads(response["result"]), indent=4))
