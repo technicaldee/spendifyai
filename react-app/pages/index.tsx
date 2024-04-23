d42f9570a274bf60c8595c83f2947ed205f0f3ac
Edidiong Udoh <gisthive@gmail.com>
2024-04-23 08:14:00 +0100

    env

--- react-app/pages/index.tsx
+++ react-app/pages/index.tsx
@@ -95,7 +95,7 @@ export default function Home() {
         {
           method: "POST",
           headers: {
-            Authorization: "Bearer " + process.env.OPENAI_API_KEY,
+            Authorization: "Bearer " + process.env.REACT_APP_OPENAI_API_KEY,
             "Content-Type": "application/json",
           },
           body: JSON.stringify(apiRequestBody),
